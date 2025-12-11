e117084fdd9ab7c8c9bb0353ce1903a3dcfea0dc
Ramakrishna-19 <ramakrishnadyavari1920@gmail.com>
2025-12-12 01:06:29 +0530

    Updated popupar item

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt='logo' />
-        <p>SHOPPER</p>
+        <p>SHOPNOW</p>
       </div>  
       <img className='nav-dropdown' onClick={dropdown_toggle} src= {nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
